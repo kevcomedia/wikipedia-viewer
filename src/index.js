73e5e20a5759b161bc5e3c5cdfffa7c629214035
Kev Comedia <kevcomedia@gmail.com>
2017-06-26 20:28:58 +0800

    Write initial styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV != 'production') {
 
 require('normalize.css');
 require('font-awesome/css/font-awesome.css');
+require('./styles.scss');
 
 require('./search.js');
 require('./random.js');
